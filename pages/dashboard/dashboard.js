302706b07ff1f7ba9f3655837681abc9df1acea2
ipmanlk <io@navinda.xyz>
2020-07-09 13:43:08 +0530

    add: routes for cards and navbar

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,7 +1,10 @@
 function updateTiles() {
+    // make router available inside iframe
+    window.loadRoute = mainWindow.loadRoute;
+    
     // show hide tiles based on privileges
     const privileges = mainWindow.tempData.privileges;
-
+    
     // hide all tiles
     $(".MODULE").hide();
 
